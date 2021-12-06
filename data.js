5d6b0ce96bd19e4da33762c672ef2d718d52ba5e
cpcharpi <52642295+cpcharpi@users.noreply.github.com>
2021-12-06 10:16:22 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,377 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-framing-great-room",
+      "name": "Framing: Great Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.202656367468858,
+        "pitch": 0.18569523672883115,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3588480290997431,
+          "pitch": 0.11523277970395185,
+          "rotation": 7.853981633974483,
+          "target": "1-framing-master-bedroom"
+        },
+        {
+          "yaw": 3.1222928960999337,
+          "pitch": 0.2187281271634376,
+          "rotation": 6.283185307179586,
+          "target": "2-framing-back-deck"
+        },
+        {
+          "yaw": 0.07042773922330348,
+          "pitch": 0.12474806556566342,
+          "rotation": 6.283185307179586,
+          "target": "3-framing-front-door"
+        },
+        {
+          "yaw": -0.27448030509361665,
+          "pitch": -0.07614487192717156,
+          "rotation": 0,
+          "target": "4-framing-upstairs-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-framing-master-bedroom",
+      "name": "Framing: Master Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.014501031584194024,
+        "pitch": 0.06885816506746423,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.173877460502519,
+          "pitch": 0.17198101998608095,
+          "rotation": 4.71238898038469,
+          "target": "0-framing-great-room"
+        },
+        {
+          "yaw": -1.9908545369987074,
+          "pitch": 0.15201174365924608,
+          "rotation": 3.141592653589793,
+          "target": "7-framing-yard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-framing-back-deck",
+      "name": "Framing: Back Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.053348215032825,
+          "pitch": 0.3544275523406064,
+          "rotation": 7.0685834705770345,
+          "target": "0-framing-great-room"
+        },
+        {
+          "yaw": 3.045744219838011,
+          "pitch": 0.07471407253402518,
+          "rotation": 0,
+          "target": "3-framing-front-door"
+        },
+        {
+          "yaw": -1.6387418234961881,
+          "pitch": 0.11095558104981684,
+          "rotation": 4.71238898038469,
+          "target": "7-framing-yard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-framing-front-door",
+      "name": "Framing: Front Door",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.3380310175237131,
+        "pitch": -0.08765614491363394,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0896321138646172,
+          "pitch": 0.24738513733516498,
+          "rotation": 0,
+          "target": "0-framing-great-room"
+        },
+        {
+          "yaw": 1.5271532336223803,
+          "pitch": 0.09832594612677958,
+          "rotation": 0.7853981633974483,
+          "target": "7-framing-yard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-framing-upstairs-hall",
+      "name": "Framing: Upstairs Hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.12613178608668285,
+        "pitch": 0.11786847298549219,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1146225976976813,
+          "pitch": 0.11369029274313291,
+          "rotation": 0,
+          "target": "6-framing-bonus-room"
+        },
+        {
+          "yaw": -0.44051273388102175,
+          "pitch": 0.21459612757872648,
+          "rotation": 4.71238898038469,
+          "target": "5-framing-upstairs-front-bedroom"
+        },
+        {
+          "yaw": -3.041268786451951,
+          "pitch": 0.33980549048969877,
+          "rotation": 2.356194490192345,
+          "target": "0-framing-great-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-framing-upstairs-front-bedroom",
+      "name": "Framing: Upstairs Front Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.968648816722503,
+        "pitch": 0.17151820030639442,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5627889646040494,
+          "pitch": 0.18722620888313024,
+          "rotation": 7.0685834705770345,
+          "target": "4-framing-upstairs-hall"
+        },
+        {
+          "yaw": -1.3674572196850967,
+          "pitch": 0.29281234389088695,
+          "rotation": 3.141592653589793,
+          "target": "0-framing-great-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-framing-bonus-room",
+      "name": "Framing: Bonus Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.0034423809137535955,
+        "pitch": 0.217354544657141,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08948101065422875,
+          "pitch": 0.2142341120558875,
+          "rotation": 0,
+          "target": "4-framing-upstairs-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-framing-yard",
+      "name": "Framing: Yard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20152025698287446,
+          "pitch": -0.05433748680709449,
+          "rotation": 0,
+          "target": "3-framing-front-door"
+        },
+        {
+          "yaw": -0.6837924429838793,
+          "pitch": -0.02237386166696176,
+          "rotation": 1.5707963267948966,
+          "target": "2-framing-back-deck"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Suncrest Way Construction 2021",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
